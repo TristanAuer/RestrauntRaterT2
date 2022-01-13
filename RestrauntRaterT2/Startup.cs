4b7067faab2eba296c7f7ce0941e0f2c6f0b0042
Tristan Auer <tristanjauer@gmail.com>
2022-01-13 11:52:16 -0500

    first commit

---
+++ RestrauntRaterT2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestrauntRaterT2.Startup))]
+namespace RestrauntRaterT2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
