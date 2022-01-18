633cc02b9dea1f8f40f3963b69bbdef226ae58c1
Tristan Auer <tristanjauer@gmail.com>
2022-01-17 21:08:27 -0500

    Post and Get for Create View

---
+++ RestrauntRaterT2/Controllers/GetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestrauntRaterT2.Controllers
+{
+    public class GetController : Controller
+    {
+        // GET: Create
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
